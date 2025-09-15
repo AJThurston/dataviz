2d8274d48983f0c89d97341e4aaebaadd46dbfd7
AJThurston <35585810+AJThurston@users.noreply.github.com>
2025-09-14 20:21:00 -0400

    Update

--- theme/theme.r
+++ theme/theme.r
@@ -1,12 +1,12 @@
+suppressMessages({
+  suppressWarnings({
+    library(showtext)
+    font_add_google("JetBrains Mono", "jetbrains")
+    showtext_auto()
+  })
+})
+
 theme_custom <- function(base_size = 16, base_family = "jetbrains") {
-  
-  library(ggplot2)
-  library(showtext)
-  
-  # Add JetBrains Mono from Google Fonts
-  font_add_google("JetBrains Mono", "jetbrains")
-  showtext_auto()
-  
   theme(
     text = element_text(size = base_size, family = base_family),
     plot.background = element_rect(fill = "#FFFFFC", color = NA),
