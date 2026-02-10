f9a42ebf2b12229ff2b982342eb07d8aed75111d
AJThurston <35585810+AJThurston@users.noreply.github.com>
2026-02-10 07:41:28 -0500

    Added nround

---
+++ functions/nround.R
@@ -0,0 +1,10 @@
+# from https://github.com/sfirke/janitor/blob/main/R/round_half_up.R
+
+nround <- function(x, digits = 0) {
+  posneg <- sign(x)
+  z <- abs(x) * 10^digits
+  z <- z + 0.5 + sqrt(.Machine$double.eps)
+  z <- trunc(z)
+  z <- z / 10^digits
+  z * posneg
+}
